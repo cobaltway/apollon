9594ad14c8d1d95de82bc4b41b83af9cb8b3e821
Cobalt <toutcobalt@gmail.com>
2017-04-19 19:26:11 +0200

    Bug fix

--- .gitupdate.js
+++ .gitupdate.js
@@ -1,9 +1,11 @@
 if (true) {
-    const GitWatch = require('git-remote-update');
+    const GitWatcher = require('git-remote-update');
 
-    const gw = new GitWatch();
+    const gw = new GitWatcher({
+        git: './.git'
+    });
 
-    gw.on('ready', gw.watch);
-    gw.on('newerCommit', gw.update);
-    gw.on('updated', process.exit);
+    gw.on('ready', () => gw.watch(120000));
+    gw.on('newerCommit', () => gw.update());
+    gw.on('updated', () => process.exit());
 }

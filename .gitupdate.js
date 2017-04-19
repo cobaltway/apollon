2dbcf83b10c8d0f6ff614340bffd2c74bc0cdccc
Cobalt <toutcobalt@gmail.com>
2017-04-19 15:15:37 +0200

    Fix webserv

--- .gitupdate.js
+++ .gitupdate.js
@@ -1,5 +1,7 @@
-const gw = new require('git-remote-update');
+if (false) {
+    const gw = new require('git-remote-update');
 
-gw.on('ready', gw.watch);
-gw.on('newerCommit', gw.update);
-gw.on('updated', process.exit);
+    gw.on('ready', gw.watch);
+    gw.on('newerCommit', gw.update);
+    gw.on('updated', process.exit);
+}

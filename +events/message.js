6bd3bc0255ba2847fe1d1f6617dcc1ea3cdf5556
Cobalt <toutcobalt@gmail.com>
2017-05-07 12:27:04 +0200

    test membercount

--- +events/message.js
+++ +events/message.js
@@ -21,7 +21,7 @@ module.exports = function(message) {
         if (this.deleteMessages(message)) {
             return;
         }
-        console.log(message.guild.memberCount, message.guild.members.keys().length);
+        console.log(message.guild.memberCount, message.guild.members.size);
     }
 
     if (this.lmgtfy(message)) {

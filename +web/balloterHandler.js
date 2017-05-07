879ce5376cab027986d0f1dab9ec98a04df21f67
Cobalt <toutcobalt@gmail.com>
2017-05-07 18:34:44 +0200

    Correct balloter

--- +web/balloterHandler.js
+++ +web/balloterHandler.js
@@ -1,5 +1,5 @@
-const request = require('request'),
-    code = global.credentials.devToken;
+const code = global.credentials.devToken,
+    bodyParser = require('body-parser');
 
 function getNumberOfUsers(guild, role) {
     let count = 0;
@@ -11,13 +11,14 @@ function getNumberOfUsers(guild, role) {
     return count;
 }
 
-function diffuseKeys({guild, name, role, channel, links}) {
+function diffuseKeys({guild, name, role, channel, tokens, slug, basePath}) {
     let count = 0;
     const guildObject = this.guilds.get(guild);
     const roleObject = guildObject.roles.find('name', role);
     guildObject.members.forEach((m) => {
+        let link = basePath + '/election/' + slug + '/' + tokens[count++];
         setTimeout(() => {
-            this.talk(m, `Voilà ton lien pour le vote \`${name}\` sur ${guildObject.name}.\nPour voter, il te suffit de cliquer et de remplir le formulaire.\nCe lien ne fonctionnera qu'une seule fois, pour ce vote seulement, et il t'est personnel, alors veille à ne pas le diffuser.\n⮕ ${links[count++]}`);
+            this.talk(m, `Voilà ton lien pour le vote \`${name}\` sur ${guildObject.name}.\nPour voter, il te suffit de cliquer et de remplir le formulaire.\nCe lien ne fonctionnera qu'une seule fois, pour ce vote seulement, et il t'est personnel, alors veille à ne pas le diffuser.\n⮕ ${link}`);
         }, 100);
     });
 
@@ -26,24 +27,41 @@ function diffuseKeys({guild, name, role, channel, links}) {
 }
 
 module.exports = function(app) {
-    app.post('/balloter/:basePath/:name/:code/:guild/:channel/:role', (req, res) => {
+    app.get('/balloter/:code/membersCount/:guild/:role/', (req, res) => {
         if (req.params.code !== code) {
             res.status(403).end();
             return;
         }
 
-        const numbers = getNumberOfUsers.call(this, req.params.guild, req.params.role);
-        request.get(`http://${req.params.basePath}/api/getKeys/${code}/${numbers}`)
-        .on('response', (response) => {
-            const links = JSON.parse(response).links;
+        res.status(200).end(String(getNumberOfUsers.call(this, req.params.guild, req.params.role)));
+    });
+
+    app.post('/balloter/:code/broadcast/:guild/:channel/:role',
+        bodyParser.urlencoded({ extended: false }),
+        (req, res) => {
+            if (req.params.code !== code) {
+                res.status(403).end();
+                return;
+            }
+            console.log('hello',
+                {
+                    guild: req.params.guild,
+                    role: req.params.role,
+                    channel: req.params.channel,
+                    tokens: JSON.parse(req.body.tokens),
+                    name: req.body.name,
+                    slug: req.body.slug,
+                    basePath: req.body.basePath
+                });
             diffuseKeys.call(this, {
                 guild: req.params.guild,
-                name: req.params.name,
                 role: req.params.role,
                 channel: req.params.channel,
-                links
+                tokens: JSON.parse(req.body.tokens),
+                name: req.body.name,
+                slug: req.body.slug,
+                basePath: req.body.basePath
             });
-            res.status(200).end('OK');
+            res.status(200).end();
         });
-    });
 };
--- +web/http.js
+++ +web/http.js
@@ -9,11 +9,13 @@ const port = global.config.web.port,
 module.exports = function() {
     ballot.call(this, httpServer);
 
+/*
     httpServer.use(require('express-basic-auth')({ // Lock the http access
         users: { [user]: password },
         challenge: true,
         realm: 'Apollon heart'
     }));
+*/
 
     httpServer.use(express.static('data')); // Serve log files
     httpServer.listen(port, () => { // Make it listen for incoming http requests

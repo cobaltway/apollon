89302bb9050c0b33b9e36d0312511704f3eeff56
Cobalt <toutcobalt@gmail.com>
2017-05-16 17:07:32 +0200

    Fixing cron tab (2)

--- +cron/everyDayAt8AM.js
+++ +cron/everyDayAt8AM.js
@@ -2,7 +2,7 @@ const cron = require('node-cron'),
     guilds = global.config.actions.calendrier.guilds;
 
 module.exports = function() {
-    cron.schedule('9 * * *', () => {
+    cron.schedule('* * 9 * * *', () => {
         guilds.forEach(g => {
             this.calendrier({
                 content: 'Quel jour sommes-nous citoyen ?',
